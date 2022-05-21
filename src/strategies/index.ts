a72c39a6fbd9a8e389305375afb57bfca466350f
mas <mas@jetprotocol.io>
2022-05-21 11:18:48 -0500

    Cleaned up old strategies.

--- src/strategies/index.ts
+++ src/strategies/index.ts
@@ -5,8 +5,6 @@ import { Configuration } from '../configuration';
 import { Oracle } from '../oracle';
 import { PositionManager } from '../positionManager';
 
-import { FixedSpread } from "./fixedSpread";
-import { Grid } from "./grid";
 import { RandomTaker } from "./randomTaker";
 import { ReplicateMainnet } from "./replicateMainnet";
 import { Strategy } from "./strategy";
@@ -19,8 +17,6 @@ export function createStrategy(
   mainnetMarket: Market,
 ): Strategy {
   switch (configuration.params.type) {
-    case 'fixed-spread': return new FixedSpread(configuration, oracle, positionManager);
-    case 'grid': return new Grid(configuration, oracle, positionManager);
     case 'random-taker': return new RandomTaker(configuration, oracle, positionManager);
     case 'replicate-mainnet': return new ReplicateMainnet(configuration, oracle, positionManager, mainnetConnection, mainnetMarket);
     default: { console.log(`Unhandled params: ${configuration.params.type}`); process.exit(); break; }
