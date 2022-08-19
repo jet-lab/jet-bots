ca58437394ff237c892c32fae45a0e334272f6fe
Partially Sorted <partiallysorted@gmail.com>
2022-08-19 10:49:35 -0500

    Added placeholder for Orca.

--- packages/bot-sdk/src/markets/index.ts
+++ packages/bot-sdk/src/markets/index.ts
@@ -1,5 +1,6 @@
 export { DriftMarket } from './drift';
 export { MangoMarket } from './mango';
 export * from './market';
+export { OrcaMarket } from './orca';
 export { SerumMarket } from './serum';
 export { ZeroOneMarket } from './zero-one';
---
+++ packages/bot-sdk/src/markets/orca/index.ts
@@ -0,0 +1,23 @@
+import { Configuration, MarketConfiguration } from '../../configuration';
+import { Connection } from '../../connection';
+import { Position } from '../../margin-accounts/position';
+import { Market } from '../market';
+
+export class OrcaMarket extends Market {
+  constructor(
+    configuration: Configuration,
+    marketConfiguration: MarketConfiguration,
+    positions: Record<string, Position>,
+    connection: Connection,
+  ) {
+    super(configuration, marketConfiguration, positions, connection);
+  }
+
+  async crank(): Promise<void> {
+    throw new Error('Implement');
+  }
+
+  async listen(): Promise<void> {
+    throw new Error('Implement');
+  }
+}
