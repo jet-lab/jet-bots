eb16c0a949a4c316ecfd4bd5df2a2fb2aff7c61c
Partially Sorted <partiallysorted@gmail.com>
2022-08-23 09:59:22 -0500

    Removed unused references.

--- packages/bot-sdk/src/markets/index.ts
+++ packages/bot-sdk/src/markets/index.ts
@@ -1,6 +1,2 @@
-export { DriftMarket } from './drift';
-export { MangoMarket } from './mango';
 export * from './market';
-export { OrcaMarket } from './orca';
 export { SerumMarket } from './serum';
-export { ZeroOneMarket } from './zero-one';
--- packages/bot-sdk/src/protocols/index.ts
+++ packages/bot-sdk/src/protocols/index.ts
@@ -1,7 +1,2 @@
-export { JetProtocol } from './jet';
-export { MangoProtocol } from './mango';
-export { MarginfiProtocol } from './marginfi';
 export { Protocol } from './protocol';
 export { SolanaProtocol } from './solana';
-export { SolendProtocol } from './solend';
-export { ZeroOneProtocol } from './zero-one';
